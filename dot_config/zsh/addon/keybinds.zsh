06c85a3f8d8dc87aada1c652d085a89433e19348
broemp <derbrommi@live.de>
2023-07-11 23:59:40 +0200

    Update .config/zsh/addon/keybinds.zsh

--- dot_config/zsh/addon/keybinds.zsh
+++ dot_config/zsh/addon/keybinds.zsh
@@ -2,3 +2,5 @@ bindkey '^H' backward-kill-word
 bindkey '^[[3;5~' kill-word
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
+bindkey  "^[[H"   beginning-of-line
+bindkey  "^[[F"   end-of-line
