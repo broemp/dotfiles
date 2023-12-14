ed3afd72719cc94b1d3459928732c0616a40c0b1
broemp <derbrommi@live.de>
2023-12-14 13:07:31 +0100

    Update .config/zsh/addon/keybinds.zsh

--- dot_config/zsh/addon/keybinds.zsh
+++ dot_config/zsh/addon/keybinds.zsh
@@ -8,3 +8,18 @@ bindkey '^H' backward-kill-word
 bindkey '5~' kill-word
 bindkey  "^[[H"   beginning-of-line
 bindkey  "^[[F"   end-of-line
+
+# keybinds
+bindkey '^ ' autosuggest-accept
+
+export PATH="$HOME/.local/bin":$PATH
+
+if command -v bat &> /dev/null; then
+  alias cat="bat -pp"
+  alias catt="bat"
+fi
+
+function chpwd() {
+    emulate -L zsh
+    ls -a
+}
