d4bea46d9d24e6de03905cb03a0fba7b3f30d02d
broemp <derbrommi@live.de>
2023-12-14 13:08:25 +0100

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -37,3 +37,13 @@ wk.register({
     },
   },
 })
+
+-- Disable Arrow keys
+-- Shorten function name
+local keymap = vim.keymap.set
+
+local opts = { noremap = true, silent = true }
+keymap("n", "<Up>", "<Nop>", opts)
+keymap("n", "<Down>", "<Nop>", opts)
+keymap("n", "<Left>", "<Nop>", opts)
+keymap("n", "<Right>", "<Nop>", opts)
