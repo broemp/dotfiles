241bf1d90a6a9c02b6564e1eefcbfdc2f6f0abe7
Julian <julian.brombach@stud.uni-due.de>
2024-01-16 11:58:24 +0100

    Update .config/zsh/addon/aliases.zsh

--- dot_config/zsh/addon/aliases.zsh
+++ dot_config/zsh/addon/aliases.zsh
@@ -3,7 +3,12 @@ alias j='z'
 alias f='zi'
 alias g='lazygit'
 alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
-alias nvimrc='nvim ~/.config/nvim/'
+if command -v chezmoi &> /dev/null
+then
+  alias nvimrc='chezmoi edit ~/.config/nvim/'
+else
+  alias nvimrc='nvim ~/.config/nvim/'
+fi
 
 # get fastest mirrors
 alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
