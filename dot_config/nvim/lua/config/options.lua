2ce4cb14e5c96703068966e762dffa6130556785
broemp <derbrommi@live.de>
2024-02-19 13:51:01 +0100

    Update .config/nvim/lua/config/autocmds.lua Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,7 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.api.nvim_create_autocmd("BufWinEnter", {
-  pattern = "*.templ",
-  command = "setlocal filetype=templ | setlocal commentstring=#%s",
-})
