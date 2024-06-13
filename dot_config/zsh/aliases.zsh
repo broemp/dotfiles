17326ddeaa5f6cbd1ab67146240b71b6e3eb520f
broemp <derbrommi@live.de>
2024-06-13 15:24:48 +0200

    Update .config/zsh/aliases.zsh

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -11,7 +11,7 @@ else
 fi
 
 # get fastest mirrors
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirror="sudo reflector -f 30 -l 30 -c AT,DE,NL,LU --number 10 --verbose --save /etc/pacman.d/mirrorlist"
 alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
 alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
 alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
